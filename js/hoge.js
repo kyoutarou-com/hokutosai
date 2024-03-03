3c427c1ae28d95c2e6f531f96287759aee32d0a8
mako0523 <kurimako.0523@gmail.com>
2024-03-03 21:01:05 +0900

    feat: ヘッダー･フッターの共通化をするための基盤を作成

--- js/hoge.js
+++ js/hoge.js
@@ -1,3 +1,28 @@
+//ヘッダー
+function header() {
+  $.ajax({
+    url: "../header.html",
+    cache: false,
+    async: false,
+    dataType: "html",
+    success: function (html) {
+      document.write(html);
+    },
+  });
+}
+//フッター
+function footer() {
+  $.ajax({
+    url: "../footer.html",
+    cache: false,
+    async: false,
+    dataType: "html",
+    success: function (html) {
+      document.write(html);
+    },
+  });
+}
+
 $(function () {
   var $header = $("#header");
   var scrollSize = 800; //超えると表示
