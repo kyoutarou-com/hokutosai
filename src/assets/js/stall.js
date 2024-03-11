266c421ae6c783521195f99df60dde661415eceb
mako0523 <kurimako.0523@gmail.com>
2024-03-11 23:30:58 +0900

    feat: タブとメニューが連携するように変更

--- src/assets/js/stall.js
+++ src/assets/js/stall.js
@@ -23,7 +23,7 @@ function GethashID(hashIDName) {
 }
 
 //タブをクリックしたら
-$(".tabs a").on("click", function () {
+$(".tabs li a").on("click", function () {
 	var idName = $(this).attr("href"); //タブ内のリンク名を取得
 	GethashID(idName); //設定したタブの読み込みと
 	return false; //aタグを無効にする
