fb88e375e2953e5fdc0d8929ba635163d911aae5
mako0523 <kurimako.0523@gmail.com>
2024-03-12 19:47:52 +0900

    feat: csvデータを元に要素の中身を書き換えるように変更

---
+++ src/assets/js/stall-individual.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const getHash = () => {
+	const hash = location.hash;
+
+	// #記号を含まないハッシュを返す．
+	return hash.substring(1);
+};
+
+const insertTextIntoElement = (id, text) => {
+	$(id).text(text);
+};
+
+const onSuccess = (json) => {
+	console.log(json);
+	const hash = getHash();
+	const index = parseInt(hash);
+
+	insertTextIntoElement("#stall-name", json[index]["stall-name"]);
+	insertTextIntoElement("#stall-image", json[index]["image"]);
+	insertTextIntoElement("#organization", json[index]["organization"]);
+	insertTextIntoElement("#location", json[index]["location"]);
+	insertTextIntoElement("#time", json[index]["time"]);
+	insertTextIntoElement("#attention", json[index]["attention"]);
+	insertTextIntoElement("#stall-comment", json[index]["stall-comment"]);
+};
+
+const onError = (error) => {
+	console.error("Error occurred while fetching data:", error);
+};
+
+$.ajax({
+	type: "GET",
+	url: "../../../dist/assets/config/stall-individual.json",
+	dataType: "json",
+	async: false,
+}).then(onSuccess, onerror);
