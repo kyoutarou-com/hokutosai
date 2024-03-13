5fd6e3f772af82397474fb6d69682142687db44e
mako0523 <kurimako.0523@gmail.com>
2024-03-13 19:08:43 +0900

    fix: タイポしていたので修正

--- src/assets/js/stall.js
+++ src/assets/js/stall.js
@@ -8,10 +8,10 @@ function getHashID_tabs(hashIDName) {
 			.find("a")
 			.each(function () {
 				//タブ内のaタグ全てを取得
-				var idName = $(this).attr("href"); //タブ内のaタグのリンク名（例）#lunchの値を取得
+				const idName = $(this).attr("href"); //タブ内のaタグのリンク名（例）#lunchの値を取得
 				if (idName == hashIDName) {
 					//リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
-					var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
+					const parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
 					$(".tabs li").removeClass("active"); //タブ内のliについているactiveクラスを取り除き
 					$(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
 					//表示させるエリア設定
@@ -29,10 +29,10 @@ function getHashID_menus(hashIDName) {
 			.find("a")
 			.each(function () {
 				//タブ内のaタグ全てを取得
-				var idName = $(this).attr("href"); //タブ内のaタグのリンク名（例）#lunchの値を取得
+				const idName = $(this).attr("href"); //タブ内のaタグのリンク名（例）#lunchの値を取得
 				if (idName == hashIDName) {
 					//リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
-					var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
+					const parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
 					$(".menus li").removeClass("active"); //タブ内のliについているactiveクラスを取り除き
 					$(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
 					//表示させるエリア設定
@@ -45,20 +45,20 @@ function getHashID_menus(hashIDName) {
 
 //タブをクリックしたら
 $(".tabs li a").on("click", function () {
-	var idName = $(this).attr("href"); //タブ内のリンク名を取得
+	const idName = $(this).attr("href"); //タブ内のリンク名を取得
 	getHashID_tabs(idName); //設定したタブの読み込みと
 	return false; //aタグを無効にする
 });
 
 $(".menus li a").on("click", function () {
-	var idName = $(this).attr("href"); //タブ内のリンク名を取得
+	const idName = $(this).attr("href"); //タブ内のリンク名を取得
 	getHashID_menus(idName); //設定したタブの読み込みと
 	return false; //aタグを無効にする
 });
 
 // 上記の動きをページが読み込まれたらすぐに動かす
-$(window).on("load", function () {
-	var hashName = location.hash; //リンク元の指定されたURLのハッシュタグを取得
+$(window).on("load", () => {
+	const hashName = location.hash; //リンク元の指定されたURLのハッシュタグを取得
 	getHashID_tabs(hashName); //設定したタブの読み込み
 	getHashID_menus(hashName);
 });
