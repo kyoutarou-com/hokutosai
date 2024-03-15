2c9cc6960a9623453eda1ce0a91eb7b7e9250647
mako0523 <kurimako.0523@gmail.com>
2024-03-15 22:28:16 +0900

    chore: 関数に分割

--- scripts/csv-to-json.sh
+++ scripts/csv-to-json.sh
@@ -5,16 +5,18 @@ INPUT_DIRECTORY="src/assets/config"
 OUTPUT_DIRECTORY="dist/assets/config"
 
 remove_file_extension() {
-
+	echo "${1%.*}"
 }
 
-# 前回の出力結果を削除
+# Delete previous output results.
 if [ -e ${OUTPUT_DIRECTORY} ]; then
 	rm -r ${OUTPUT_DIRECTORY}
 fi
 
 mkdir -p ${OUTPUT_DIRECTORY}
 
-for target_file in ${TARGET_FILES[@]}; do
-	npx csvtojson ${INPUT_DIRECTORY}/${target_file} >${OUTPUT_DIRECTORY}/${target_file%.*}.json
+for target_file in "${TARGET_FILES[@]}"; do
+	target_file_without_extension="$(remove_file_extension "${target_file}")"
+	npx csvtojson "${INPUT_DIRECTORY}/${target_file}" \
+		>"${OUTPUT_DIRECTORY}/${target_file_without_extension}.json"
 done
