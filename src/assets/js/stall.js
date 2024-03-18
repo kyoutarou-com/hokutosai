76c659ad6f3b2670f6f62c7c42896bfe50d227c7
mako0523 <kurimako.0523@gmail.com>
2024-03-18 10:27:08 +0900

    fix: 使わないフィールドを削除

--- src/assets/js/stall.js
+++ src/assets/js/stall.js
@@ -2,65 +2,7 @@
 
 import $ from "jquery";
 import { loadJson, onError } from "./module/json.js";
-
-const getTabId = (menuId) => {
-	let tabId;
-	switch (menuId) {
-		case "#product1":
-		case "#product2":
-		case "#product3":
-		case "#product4":
-			tabId = "#tab1";
-			break;
-		case "#location1":
-		case "#location2":
-		case "#location3":
-		case "#location4":
-			tabId = "#tab2";
-			break;
-		case "#organization1":
-		case "#organization2":
-		case "#organization3":
-		case "#organization4":
-			tabId = "#tab3";
-			break;
-		default:
-			// menuIdが無効の値の場合最初のタブのidを代入
-			tabId = "#tab1";
-			break;
-	}
-	return tabId;
-};
-
-const selectTab = (tabId) => {
-	$(".tabs li").removeClass("active");
-	const index = tabId.substring(4);
-	$(`.tabs li:nth-child(${index})`).addClass("active");
-	$(".tab-area").removeClass("is-active");
-	$(tabId).addClass("is-active");
-};
-
-const selectMenu = (menuId) => {
-	if (!menuId) {
-		menuId = "#product1";
-	}
-	const tabId = getTabId(menuId);
-	selectTab(tabId);
-
-	// メニューの選択;
-	$(".menus li")
-		.find("a")
-		.each(function () {
-			const id = $(this).attr("href");
-			if (id === menuId) {
-				const parent = $(this).parent();
-				$(".menus li").removeClass("active");
-				$(parent).addClass("active");
-				$(".menu-area").removeClass("is-active");
-				$(menuId).addClass("is-active");
-			}
-		});
-};
+import { selectMenu } from "./module/tab.js";
 
 $(".tabs li a , .menus li a").on("click", function () {
 	const menuId = $(this).attr("href");
@@ -82,7 +24,7 @@ const createStall = (id, json) => {
 				<h3 class="stall-name angle-2-box">${json["stall-name"]}</h3>
 				<ul>
 					<li class="stall-organization">${json["stall-organization"]}</li>
-					<li class="stall-category">${json["stall-category"]}</li>
+					<li class="stall-category">${json["stall-place"]}</li>
 				</ul>
 			</div>
 		</div>
