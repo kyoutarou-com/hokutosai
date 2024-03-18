d3e8e1735168c88e2bf2351281f04494dc5360c9
mako0523 <kurimako.0523@gmail.com>
2024-03-18 10:55:17 +0900

    fix: クラス名修正

--- src/assets/js/stall-individual.js
+++ src/assets/js/stall-individual.js
@@ -8,11 +8,11 @@ const onSuccess = (json) => {
 	const stallIndex = getPageIndex();
 
 	insertTextIntoElement("#stall-name", json[stallIndex]["stall-name"]);
-	insertValueIntoElement(".stall-image", "src", json[stallIndex]["stall-image"]);
-	insertTextIntoElement("#organization", json[stallIndex]["stall-organization"]);
-	insertTextIntoElement("#location", json[stallIndex]["stall-location"]);
-	insertTextIntoElement("#time", json[stallIndex]["time"]);
-	insertTextIntoElement("#attention", json[stallIndex]["attention"]);
+	insertValueIntoElement("#stall-image", "src", json[stallIndex]["stall-image"]);
+	insertTextIntoElement("#stall-organization", json[stallIndex]["stall-organization"]);
+	insertTextIntoElement("#stall-location", json[stallIndex]["stall-location"]);
+	insertTextIntoElement("#stall-time", json[stallIndex]["time"]);
+	insertTextIntoElement("#stall-attention", json[stallIndex]["attention"]);
 	insertTextIntoElement("#stall-comment", json[stallIndex]["stall-comment"]);
 };
 
