f4b0880afa481d057c7891089b7dcf6a0179fea6
kyoutarou-com <1014kyo1014@gmail.com>
2024-03-31 11:08:39 +0900

    fix:スクロール位置調整

--- src/assets/js/theme.js
+++ src/assets/js/theme.js
@@ -15,31 +15,6 @@ $(window).on("load resize", function () {
 
 // blurTriggerにblurというクラス名を付ける定義
 
-// function BlurTextAnimeControl() {
-// 	$(".blurTrigger").each(function () {
-// 		//blurTriggerというクラス名が
-// 		var elemPos = $(this).offset().top - 50; //要素より、50px上の
-// 		var scroll = $(window).scrollTop();
-// 		var windowHeight = $(window).height();
-// 		if (scroll >= elemPos - windowHeight) {
-// 			$(this).addClass("blur");
-// 		}, 3000); // 画面内に入ったらblurというクラス名を追記
-// 		} else {
-// 			$(this).removeClass("blur"); // 画面外に出たらblurというクラス名を外す
-// 		}
-// 	});
-// }
-
-// // 画面をスクロールをしたら動かしたい場合の記述
-// $(window).scroll(function () {
-// 	BlurTextAnimeControl(); /* アニメーション用の関数を呼ぶ*/
-// }); // ここまで画面をスクロールをしたら動かしたい場合の記述
-
-// // 画面が読み込まれたらすぐに動かしたい場合の記述
-// $(window).on("load", function () {
-// 	BlurTextAnimeControl(); /* アニメーション用の関数を呼ぶ*/
-// }); // ここまで画面が読み込まれたらすぐに動かしたい場合の記述
-
 function BlurTextAnimeControl() {
 	$(".blurTrigger").each(function () {
 		var elemPos = $(this).offset().top - 50; // 要素より、50px上の
@@ -60,3 +35,30 @@ function BlurTextAnimeControl() {
 $(window).scroll(function () {
 	BlurTextAnimeControl(); /* アニメーション用の関数を呼ぶ*/
 });
+
+// PROTEENのメインロゴ
+function BlurTextAnimeControl() {
+	$(".blurTrigger-logo").each(function () {
+		var elemPos = $(this).offset().top - 50; // 要素より、50px上の
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		var $element = $(this);
+		if (scroll >= elemPos - windowHeight) {
+			setTimeout(function () {
+				$element.addClass("blur"); // 画面内に入ってから3秒後にblurクラスを追加
+			}, 2000);
+		} else {
+			$element.removeClass("blur"); // 画面外に出たらblurというクラス名を外す
+		}
+	});
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	BlurTextAnimeControl(); /* アニメーション用の関数を呼ぶ*/
+});
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on("load", function () {
+	slideAnime(); /* アニメーション用の関数を呼ぶ*/
+}); // ここまで画面が読み込まれたらすぐに動かしたい場合の記述
