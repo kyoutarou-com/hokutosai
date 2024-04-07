c75cc40e24c0268b064a37a19b36cae594890345
mako0523 <kurimako.0523@gmail.com>
2024-04-08 06:29:45 +0900

    feat: 複数端末の投票を防ぐため，スマホのみ投票できるように変更

--- src/assets/js/vote.js
+++ src/assets/js/vote.js
@@ -3,68 +3,82 @@
 import $ from "jquery";
 import Cookies from "js-cookie";
 
-const onSuccess = (data) => {
-	console.log(data);
-
-	if (Cookies.get("isVoted")) {
-		window.alert("投票は1回だけだぞ");
-		return;
-	}
-
-	Cookies.set("isVoted", true, { expires: 7 });
-
+const onVoteSuccess = (data) => {
+	Cookies.set("isVoted", true, { expires: 2 });
 	location.href = $("#vote-button").attr("href");
 };
 
-const onFailure = () => {
-	console.log("失敗");
+const onVoteError = (data) => {
+	throw new data();
 };
 
-const getVoteData = () => {
-	const voteData = $(":checked").data("name");
-	console.log(voteData);
-	return voteData;
-};
-
-const vote = (name) => {
+const vote = (selectedVote) => {
 	$.ajax({
-		url: `https://hokutofes.com/api/vote/${name}`,
+		url: `https://hokutofes.com/api/vote/${selectedVote}`,
 		method: "PUT",
 		xhrFields: {
 			withCredentials: true,
 		},
-		success: onSuccess,
-		error: onFailure,
+		success: onVoteSuccess,
+		error: onVoteError,
 	});
+};
+
+const isSmartphone = () => {
+	if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+		return true;
+	}
+	return false;
+};
+
+const isVoteTime = () => {
+	const currentTime = new Date();
+	const firstTime = new Date(2024, 4, 25, 14);
+	const lastTime = new Date(2024, 4, 26, 10);
+
+	const isVoteTime = currentTime < firstTime || currentTime > lastTime;
+	if (isVoteTime) {
+		return true;
+	}
+
+	return false;
+};
+
+const isVoted = () => {
+	if (Cookies.get("isVoted")) {
+		return true;
+	}
+	return false;
+};
+
+const selectVote = () => {
+	const selectedVote = $(":checked").data("name");
+	return selectedVote;
 };
 
 $("#vote-button").on("click", (event) => {
 	event.preventDefault();
 
-	const name = getVoteData();
-	if (!name) {
+	if (!isSmartphone()) {
+		window.alert("スマホにしろ");
+		return;
+	}
+
+	if (!isVoteTime()) {
+		window.alert("投票時間外だぞ");
+		return;
+	}
+
+	if (isVoted()) {
+		window.alert("投票は1回だけだぞ");
+		return;
+	}
+
+	const selectedVote = selectVote();
+	if (!selectedVote) {
 		window.alert("選択されていません");
 		return;
 	}
 
-	vote(name);
+	vote(selectedVote);
 });
-
-// 現在の日付と時間を取得
-const currentDate = new Date();
-
-console.log("現在の日付と時間:", currentDate);
-
-// 許可された時間帯を設定
-const startTime = new Date(currentDate.getFullYear(), 4, 25, 14); // 5月25日14時
-const endTime = new Date(currentDate.getFullYear(), 4, 26, 10); // 5月26日10時
-
-console.log("許可された時間帯開始:", startTime);
-console.log("許可された時間帯終了:", endTime);
-
-// 現在の時間が許可された時間帯内かどうかをチェック
-if (currentDate < startTime || currentDate > endTime) {
-	// 許可された時間帯外の場合はアラートを表示して送信ボタンを無効化
-	alert("送信ボタンは5月25日14時から5月26日10時までの間しか押せません。");
-	document.getElementById("vote-button").disabled = true; // 送信ボタンを無効化
-}
