2b6b995ad647fcc2d9673d27f8aaa78ec75e2516
mako0523 <kurimako.0523@gmail.com>
2024-04-13 00:29:00 +0900

    feat: 各コンテストの投票感謝ページを作成

--- src/assets/js/vote-thanks.js
+++ src/assets/js/vote-thanks.js
@@ -4,7 +4,8 @@ import $ from "jquery";
 import { loadJson, onError } from "./module/json.js";
 
 const onSuccess = (json) => {
-	$(".name").text(json[0]["name"]);
+	const data = json.find((obj) => obj.id === location.hash.substring(1));
+	$(".name").text(data["name"]);
 };
 
 loadJson("../../../dist/assets/config/vote.json", onSuccess, onError);
