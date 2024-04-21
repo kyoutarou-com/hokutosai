77681cc1be218147b55bd43e8ffc9721cf479cf0
mako0523 <kurimako.0523@gmail.com>
2024-04-21 18:20:31 +0900

    feat: 開催日時を差し込む機能を追加

--- src/assets/js/stage-individual.js
+++ src/assets/js/stage-individual.js
@@ -7,12 +7,24 @@ import { insertTextIntoElement, insertValueIntoElement } from "./module/dom.js";
 const onSuccess = (json) => {
 	const stallIndex = getPageIndex();
 	console.log(json);
+	let date;
+	switch (json[stallIndex]["tab"]) {
+		case "1":
+			date = "１日目";
+			break;
+		case "2":
+			date = "２日目";
+			break;
+		default:
+			break;
+	}
 
 	insertTextIntoElement("#stall-name-pan", json[stallIndex]["stall-name"]);
 	insertTextIntoElement("#stall-name", json[stallIndex]["stall-name"]);
 	insertValueIntoElement("#stall-image", "src", json[stallIndex]["stall-image"]);
 	insertTextIntoElement("#stall-organization", json[stallIndex]["stall-organization"]);
 	insertTextIntoElement("#stall-location", json[stallIndex]["stall-location"]);
+	insertTextIntoElement("#stall-date", date);
 	insertTextIntoElement("#stall-time", json[stallIndex]["time"]);
 	insertTextIntoElement("#stall-attention", json[stallIndex]["attention"]);
 	insertTextIntoElement("#stall-comment", json[stallIndex]["stall-comment"]);
