63e2f951db73b1a9e931d0922e2f5a0637b8d2ce
mako0523 <kurimako.0523@gmail.com>
2024-04-22 23:58:26 +0900

    feat: 展示個別ページにが画像のスライドショー追加

--- src/assets/js/exhibition-individual.js
+++ src/assets/js/exhibition-individual.js
@@ -6,15 +6,31 @@ import { insertTextIntoElement, insertValueIntoElement } from "./module/dom.js";
 
 const onSuccess = (json) => {
 	const stallIndex = getPageIndex();
+	const image2 = json[stallIndex]["stall-image2"];
+	const image3 = json[stallIndex]["stall-image3"];
+	const image4 = json[stallIndex]["stall-image4"];
 
 	insertTextIntoElement("#stall-name-pan", json[stallIndex]["stall-name"]);
 	insertTextIntoElement("#stall-name", json[stallIndex]["stall-name"]);
-	insertValueIntoElement("#stall-image", "src", json[stallIndex]["stall-image1"]);
+	insertValueIntoElement("#stall-image1", "src", json[stallIndex]["stall-image1"]);
+	insertValueIntoElement("#stall-image2", "src", json[stallIndex]["stall-image2"]);
+	insertValueIntoElement("#stall-image3", "src", json[stallIndex]["stall-image3"]);
+	insertValueIntoElement("#stall-image4", "src", json[stallIndex]["stall-image4"]);
 	insertTextIntoElement("#stall-organization", json[stallIndex]["stall-organization"]);
 	insertTextIntoElement("#stall-location", json[stallIndex]["stall-location"]);
 	insertTextIntoElement("#stall-time", json[stallIndex]["time"]);
 	insertTextIntoElement("#stall-attention", json[stallIndex]["attention"]);
 	insertTextIntoElement("#stall-comment", json[stallIndex]["stall-comment"]);
+
+	if (image2 === "null") {
+		$("#stall-image2").remove();
+	}
+	if (image3 === "null") {
+		$("#stall-image3").remove();
+	}
+	if (image4 === "null") {
+		$("#stall-image4").remove();
+	}
 };
 
 loadJson("assets/config/exhibition.json", onSuccess, onError);
