291a497dd516e0a663a87d296c83897662d75627
mako0523 <kurimako.0523@gmail.com>
2024-05-12 21:15:44 +0900

    feat: ローディング機能を追加

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,5 +1,8 @@
 "use strict";
 
+import $ from "jquery";
+import Vivus from "vivus";
+
 function BgFadeAnime() {
 	$(".bgLRextendTrigger").each(function () {
 		const elemPos = $(this).offset().top - 50;
@@ -24,10 +27,27 @@ function BgFadeAnime() {
 	});
 }
 
+const stroke = new Vivus(
+	"mask",
+	{
+		start: "manual",
+		type: "scenario-sync",
+		duration: 10,
+		forceRender: false,
+		animTimingFunction: Vivus.EASE,
+	},
+	() => {
+		$("#mask").attr("class", "done");
+	}
+);
+
 $(window).scroll(() => {
 	BgFadeAnime(); /* アニメーション用の関数を呼ぶ*/
 });
 
 $(window).on("load", () => {
+	$("#splash").delay(3000).fadeOut("slow");
+	$("#splash_logo").delay(3000).fadeOut("slow");
+	stroke.play();
 	BgFadeAnime();
 });
