c31f54e395a1079b9f85773cfec3a8eb11eef846
mako0523 <kurimako.0523@gmail.com>
2024-05-22 06:47:45 +0900

    feat: データを正規化する機能を追加

--- src/assets/js/where.js
+++ src/assets/js/where.js
@@ -4,7 +4,7 @@ import $ from "jquery";
 
 const convertSeparatedStringToNumber = (str, separator) => {
 	if (str === "null") {
-		return undefined;
+		return [];
 	}
 	return str.split(separator).map((str) => Number(str));
 };
@@ -14,12 +14,28 @@ const fetchJson = (url) => fetch(url).then((result) => result.json());
 const fetchEventData = async () => {
 	const urls = ["./assets/config/where.json", "./assets/config/stall.json", "./assets/config/exhibition.json"];
 	const [where, stall, exhibition] = await Promise.all(urls.map((url) => fetchJson(url)));
+	const currentLocation = where.filter((element) => `#${element.urlHash}` === location.hash)[0];
 	["sameFloorStall", "sameFloorExhibition", "closeExhibition"].map((key) => {
-		where.forEach((element, index) => {
-			where[index][key] = convertSeparatedStringToNumber(element[key], "-");
-		});
+		const closeEvents = convertSeparatedStringToNumber(currentLocation[key], "-");
+		if (!closeEvents) {
+			currentLocation[key] = [];
+		}
+		switch (key) {
+			case "sameFloorStall":
+				currentLocation[key] = closeEvents.map((index) => stall[index]);
+				break;
+			case "sameFloorExhibition":
+			case "closeExhibition":
+				currentLocation[key] = closeEvents.map((index) => exhibition[index]);
+				break;
+		}
 	});
-	return { where, stall, exhibition };
+	return currentLocation;
 };
 
-console.log(await fetchEventData());
+const eventData = await fetchEventData();
+console.log(eventData);
+
+eventData.where[0].sameFloorStall.map((index) => {
+	console.log(eventData.stall[index]);
+});
