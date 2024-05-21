314b77c90a2edf8e3448bc9af5b460d07e249a02
mako0523 <kurimako.0523@gmail.com>
2024-05-22 05:44:52 +0900

    feat: データを取得するコードを追加

--- src/assets/js/where.js
+++ src/assets/js/where.js
@@ -1,1 +1,25 @@
 "use strict";
+
+import $ from "jquery";
+
+const convertSeparatedStringToNumber = (str, separator) => {
+	if (str === "null") {
+		return undefined;
+	}
+	return str.split(separator).map((str) => Number(str));
+};
+
+const fetchJson = (url) => fetch(url).then((result) => result.json());
+
+const fetchEventData = async () => {
+	const urls = ["./assets/config/where.json", "./assets/config/stall.json", "./assets/config/exhibition.json"];
+	const [where, stall, exhibition] = await Promise.all(urls.map((url) => fetchJson(url)));
+	["sameFloorStall", "sameFloorExhibition", "closeExhibition"].map((key) => {
+		where.forEach((element, index) => {
+			where[index][key] = convertSeparatedStringToNumber(element[key], "-");
+		});
+	});
+	return { where, stall, exhibition };
+};
+
+console.log(await fetchEventData());
