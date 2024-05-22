0df0d6d136f58074b872ef6f13056a0c8a6b107f
mako0523 <kurimako.0523@gmail.com>
2024-05-22 09:48:15 +0900

    refactor: 現在地の情報を正規化する処理を関数化

--- src/assets/js/where.js
+++ src/assets/js/where.js
@@ -2,24 +2,23 @@
 
 import $ from "jquery";
 
-const convertSeparatedStringToNumber = (str, separator) => {
-	if (str === "null") {
-		return [];
-	}
-	return str.split(separator).map((str) => Number(str));
-};
-
 const fetchJson = (url) => fetch(url).then((result) => result.json());
 
 const fetchCurrentLocation = async () => {
 	const urls = ["./assets/config/where.json", "./assets/config/stall.json", "./assets/config/exhibition.json"];
-	const [where, stall, exhibition] = await Promise.all(urls.map((url) => fetchJson(url)));
+	return await Promise.all(urls.map((url) => fetchJson(url)));
+};
+
+const convertSeparatedStringToNumber = (str, separator) => {
+	if (str === "null") return [];
+
+	return str.split(separator).map((str) => Number(str));
+};
+
+const normalizeCurrentLocation = ([where, stall, exhibition]) => {
 	const currentLocation = where.filter((element) => `#${element.urlHash}` === location.hash)[0];
 	["sameFloorStall", "sameFloorExhibition", "closeExhibition"].map((key) => {
 		const closeEvents = convertSeparatedStringToNumber(currentLocation[key], "-");
-		if (!closeEvents) {
-			currentLocation[key] = [];
-		}
 		switch (key) {
 			case "sameFloorStall":
 				currentLocation[key] = closeEvents.map((index) => stall[index]);
@@ -33,7 +32,8 @@ const fetchCurrentLocation = async () => {
 	return currentLocation;
 };
 
-const currentLocation = await fetchCurrentLocation();
+const currentLocation = normalizeCurrentLocation(await fetchCurrentLocation());
+
 console.log(currentLocation);
 
 $("#location").text(currentLocation.location);
