cbc8d7bda18d442753386e50e76a00fe7c6eeb24
mako0523 <kurimako.0523@gmail.com>
2024-05-22 17:21:30 +0900

    feat: ここどこページのQRコードを生成するコードを追加

---
+++ scripts/qr.py
@@ -0,0 +1,15 @@
+import pyqrcode
+import json
+
+DOMAIN = 'hokutosai.net'
+OUTPUT_DIR = 'qr-code'
+
+with open('./dist/assets/config/where.json') as f:
+    locations = json.load(f)
+
+for location in locations:
+    hash = location['urlHash']
+    url = f'https://{DOMAIN}/where.html#{hash}'
+    file = f'{OUTPUT_DIR}/hokutosai-qr-{hash}.svg'
+    qr_code = pyqrcode.create(url)
+    qr_code.svg(file, scale=8, background='#fff')
