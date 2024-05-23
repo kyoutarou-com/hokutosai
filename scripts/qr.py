8875031c5c73d27b3a8e7082149acb918539c272
mako0523 <kurimako.0523@gmail.com>
2024-05-23 21:12:45 +0900

    fix: 透過させるように変更

--- scripts/qr.py
+++ scripts/qr.py
@@ -10,6 +10,6 @@ with open('./dist/assets/config/where.json') as f:
 for location in locations:
     hash = location['urlHash']
     url = f'https://{DOMAIN}/where.html#{hash}'
-    file = f'{OUTPUT_DIR}/hokutosai-qr-{hash}.svg'
+    file = f'{OUTPUT_DIR}/qr-{hash}.svg'
     qr_code = pyqrcode.create(url)
-    qr_code.svg(file, scale=8, background='#fff')
+    qr_code.svg(file, scale=8)
